6cc3bc562364f606b8700bd9a012f1555216486f
Sebastian Echazú <sebastianechazu@outlook.com>
2021-03-01 21:30:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    //  CalcularAreaRec1 calcularAreaRec1 = new CalcularAreaRec1();
+        
+        int base = 3; 
+        int altura = 4;
+       
+        int resultadoRec = base * altura;
+        
+        int resultadoTriRec = resultadoRec / 2;
+            
+        System.out.println("Resultado: " + resultadoRec);    
+        System.out.println("Resultado: " + resultadoTriRec);
+       
+  }
+}
